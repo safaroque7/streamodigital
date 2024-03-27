9b1e818d6025bdfecbf6068920d66f21cc8117da
safaroque7 <faroque.computer@gmail.com>
2024-03-27 17:39:56 +0600

    pricing

---
+++ pricing.php
@@ -0,0 +1,150 @@
+<?php include_once('header.php');?>
+
+<div class="container-fluid bg-streamo">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-12 mx-auto text-center text-white mb-md-5 mb-2">
+                <div class="display-3 fw-bold  pb-md-4 pb-3"> Music Distribution Pricing </div>
+                <h3> Choose your best plan </h3>
+            </div>
+
+            <div class="row">
+                <p class="text-center text-white"> All prices in USD.</p>
+            </div>
+            <div class="row mb-md-5 my-3 px-0">
+
+                <!-- free pricing -->
+                <div class="col-md-4 col-12 rounded-3 mb-md-0 mb-4">
+                    <div class="d-flex flex-column text-center bg-dark rounded-bottom rounded-3">
+
+                        <div
+                            class="d-flex flex-column py-2 px-md-4 px-2 bg-white rounded-3 pt-md-4 pt-2 h-515">
+                            <h3> Starter </h3>
+
+                            <div class="fw-bold h1 mb-md-3 mb-2 position-relative free-div mx-auto"> Free
+                                <div
+                                    class="year-text mb-0 text-dark position-absolute top-0 start-100 translate-middle">
+                                    /yr</div>
+                            </div>
+
+                            <div class="border border-secondary rounded-2 start-free-trial mx-auto mb-md-3 mb-2">
+                                <a href="https://app.streamodigital.com/register"
+                                    class="px-3 py-2 text-decoration-none text-dark d-block">Start free trial</a>
+                            </div>
+                            <p> Distribution and copyright on TikTok, Facebook/Insta, Snap, & Youtube </p>
+                            <p> Upload one song per month </p>
+                            <p> You keep 85% of royalties </p>
+                            <p> 24 hour customer service </p>
+                            <p> No customize label name </p>
+                            <p> Upgrade to Streamo for Artist for Content ID </p>
+                        </div>
+
+
+
+
+                        <div class="w-auto">
+                            <a href="https://app.streamodigital.com/register"
+                                class="text-white py-md-2 py-1 d-block text-decoration-none">
+                                Buy Starter <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                    fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd"
+                                        d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
+                                </svg> </a>
+                        </div>
+                    </div>
+                </div>
+
+
+                <!-- for artist -->
+                <div class="col-md-4 col-12 rounded-3 mb-md-0 mb-4">
+                    <div class="d-flex flex-column text-center bg-dark rounded-bottom rounded-3">
+
+                        <div
+                            class="d-flex flex-column justify-content-center py-2 px-md-4 px-2 bg-white rounded-3 pt-md-4 pt-2 h-515">
+                            <h3> For Artist </h3>
+
+                            <div class="fw-bold h1 mb-md-3 mb-2 position-relative free-div mx-auto"> $9.99
+                                <div
+                                    class="year-text mb-0 text-dark position-absolute top-0 start-100 translate-middle">
+                                    /yr</div>
+                            </div>
+
+                            <div class="border border-secondary rounded-2 start-free-trial mx-auto mb-md-3 mb-2">
+                                <a href="https://app.streamodigital.com/register"
+                                    class="px-3 py-2 text-decoration-none text-dark d-block">Start free trial</a>
+                            </div>
+                            <p> Distribution and Copyright on all major 250+ platforms. (ex. Spotify, Apple Music,
+                                Amazon Music, Tidal, Apple Music, Deezer etc.) </p>
+                            <p> Unlimited Uploads </p>
+                            <p> Dedicated artist support, Live Chat </p>
+                            <p> You keep 100% of royalties </p>
+                            <p> Customize label name </p>
+                            <p> Playlist pitching and marketing support </p>
+                            <p> No extra charge for Content Id </p>
+                        </div>
+
+
+                        <div class="w-auto">
+                            <a href="https://app.streamodigital.com/register"
+                                class="text-white py-md-2 py-1 d-block text-decoration-none">
+                                Buy Starter <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                    fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd"
+                                        d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
+                                </svg> </a>
+                        </div>
+                    </div>
+                </div>
+
+
+                <!-- Record Label -->
+                <div class="col-md-4 col-12 rounded-3 mb-md-0 mb-4">
+                    <div class="d-flex flex-column text-center bg-dark rounded-bottom rounded-3">
+
+                        <div
+                            class="d-flex flex-column py-2 px-md-4 px-2 bg-white rounded-3 pt-md-4 pt-2 h-515">
+                            <h3> Starter </h3>
+
+                            <div class="fw-bold h1 mb-md-3 mb-2 position-relative free-div mx-auto"> $49.99
+                                <div
+                                    class="year-text mb-0 text-dark position-absolute top-0 start-100 translate-middle">
+                                    /yr</div>
+                            </div>
+
+                            <div class="border border-secondary rounded-2 start-free-trial mx-auto mb-md-3 mb-2">
+                                <a href="https://app.streamodigital.com/register"
+                                    class="px-3 py-2 text-decoration-none text-dark d-block">Start free trial</a>
+                            </div>
+
+                            <p> Start free trial </p>
+                            <p> 20 artists </p>
+                            <p> Tailored service </p>
+                            <p> Dedicated account management </p>
+                            <p> Priority support </p>
+                            <p> Pitching assistance </p>
+                            <p> Early access to new features </p>
+
+                        </div>
+
+
+
+
+                        <div class="w-auto">
+                            <a href="https://app.streamodigital.com/register"
+                                class="text-white py-md-2 py-1 d-block text-decoration-none">
+                                Buy Starter <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                    fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd"
+                                        d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
+                                </svg> </a>
+                        </div>
+                    </div>
+                </div>
+
+
+
+            </div>
+        </div>
+    </div>
+</div>
+<?php include_once('footer.php');?>
