361240417e43764a1ad8f053c5d9591da6847952
safaroque7 <faroque.computer@gmail.com>
2024-03-27 16:00:05 +0600

    faq

---
+++ faq.php
@@ -0,0 +1,125 @@
+<?php include_once('header.php');?>
+
+<div class="container-fluid bg-streamo">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7 col-12 mx-auto">
+
+                <div class="display-3 text-center fw-bold text-white pb-md-4 pb-3"> Frequently Asked Questions </div>
+
+
+                <div class="accordion bg-faq mb-md-5 mb-3" id="accordionExample">
+
+                    <!-- per item -->
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingTwo">
+                            <button class="accordion-button text-white bg-faq collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                                How Do I Upload My Music to STREAMO DIGITAL?
+                            </button>
+                        </h2>
+                        <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+                            data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                            To upload your music to STREAMO DIGITAL, you’ll need to create an account and log in to
+                                our platform. Once you’re logged in, you can follow our user-friendly step-by-step
+                                process to upload your tracks and album artwork. We’ll guide you through the necessary
+                                information, such as track titles, artist names, and release dates. After completing the
+                                upload process, your music will go through our review and distribution process, and it
+                                will soon be available on various streaming platforms.
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <!-- per item -->
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingTwo">
+                            <button class="accordion-button text-white bg-faq collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                What Types of Music Can I Distribute with STREAMO DIGITAL?
+                            </button>
+                        </h2>
+                        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+                            data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                STREAMO DIGITAL accepts a wide range of music genres and types, including singles, EPs,
+                                and albums. Whether you’re an independent artist, a band, or a record label, you can use
+                                STREAMO DIGITAL to distribute your music to major streaming services and digital stores.
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- per item -->
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingThree">
+                            <button class="accordion-button text-white bg-faq collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                Can I Edit My Music Information After Uploading?
+                            </button>
+                        </h2>
+                        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
+                            data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                Yes, you can edit your music information after uploading. If you need to make changes to
+                                track titles, artist names, or any other metadata, simply log in to your STREAMO DIGITAL
+                                account, find the release you want to edit, and make the necessary updates. Keep in mind
+                                that some changes may require you to resubmit your release for distribution.
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- per item -->
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingThree">
+                            <button class="accordion-button text-white bg-faq collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                Do I Retain Ownership of My Music Rights?
+                            </button>
+                        </h2>
+                        <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingThree"
+                            data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                            Absolutely. When you distribute your music through STREAMO DIGITAL, you retain full ownership of your music rights. We act as a distribution platform to help you get your music out to the world, but you maintain control over your intellectual property.
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <!-- per item -->
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingThree">
+                            <button class="accordion-button text-white bg-faq collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                                What Happens if My Music Is Flagged for Copyright Issues?
+                            </button>
+                        </h2>
+
+                        <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingThree"
+                            data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                            If your music is flagged for copyright issues, STREAMO DIGITAL will guide you through the necessary steps to resolve the problem. We take copyright matters seriously and work to ensure that your music complies with all legal requirements. This may involve providing proof of ownership or obtaining the necessary licenses for cover songs.
+                            </div>
+                        </div>
+
+
+
+                        
+                    </div>
+
+
+
+
+
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+
+
+
+
+</div>
+
+
+<?php include_once('footer.php');?>
