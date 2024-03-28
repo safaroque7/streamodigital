9bd7cf6e077e50eff244a0b53f18b036a315069c
safaroque7 <faroque.computer@gmail.com>
2024-03-28 15:26:19 +0600

    contact

--- contact.php
+++ contact.php
@@ -2,10 +2,43 @@
 
 <div class="container-fluid bg-streamo">
     <div class="container">
+
+        <div class="row text-center mb-md-5 mb-2">
+            <div class="fw-bold mb-md-5 mb-2 pt-md-5 pt-2 text-streamo text-white fw-normal"> Get in Touch </div>
+            <div class="display-1 fw-bold mb-md-5 mb-2 text-streamo text-white"> Let’s talk </div>
+        </div>
+
+
         <div class="row">
-            <div class="col-md-7 col-12 mx-auto">
-                
+
+            <div class="col-md-6 text-white">
+                <h5 class="fw-bold pb-md-3 pb-2"> Address: </h5>
+                <p> House 53/55, Flat 7C, Block- B, </p>
+                <p> Road-3Niketon Society, Gulshan, Dhaka </p>
+                <p> Email: info@streamodigital.com </p>
+                <p> Email: support@streamodigital </p>
+                <p> Phone: 01998506523 </p>
+
             </div>
+            <div class="col-md-6 col-12 mb-md-4 mb-2 border-white border px-3 py-4 shadow p-3 mb-5 rounded-3">
+                <form>
+                    <div class="form-group mb-md-3 mb-2">
+                        <label for="yourName" class="text-white form-label">Your Name</label>
+                        <input type="text" class="form-control" id="yourName">
+                    </div>
+                    <div class="form-group mb-md-3 mb-2">
+                        <label for="yourEmail" class="text-white form-label"> Your Email </label>
+                        <input type="email" class="form-control" id="yourEmail">
+                    </div>
+                    <div class="form-group mb-md-3 mb-2">
+                        <label for="yourEmail" class="text-white form-label"> Your Comment </label>
+                        <textarea name="" id="" cols="30" rows="10" class="w-100"></textarea>
+                    </div>
+
+                    <button type="button" class="btn btn-dark border-white border-2"> Submit </button>
+                </form>
+            </div>
+
         </div>
     </div>
 
